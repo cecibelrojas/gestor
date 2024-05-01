6c80367efbf1b815423600de9ac205dbd56d978d
Cecibel Rojas <40184817+cecibelrojas@users.noreply.github.com>
2024-05-01 00:34:31 -0400

    Update index.blade.php

--- resources/views/rss/index.blade.php
+++ resources/views/rss/index.blade.php
@@ -9,7 +9,16 @@
         <description><![CDATA[ Ministerio del Poder Popular para Relaciones Exteriores ]]></description>
         <language>en</language>
         <pubDate>{{ now() }}</pubDate>
-  
+      
+      <image>
+        <url>https://portal.mppre.gob.ve/images/ico/favicon1.png</url>
+        <title>MPPRE</title>
+        <link>https://portal.mppre.gob.ve</link>
+        <width>32</width>
+        <height>32</height>
+      </image> 
+
+
         @foreach($lista as $post)
             <item>
                 <title><![CDATA[{{ $post->nombre }}]]></title>
@@ -17,6 +26,7 @@
                 <author><![CDATA[{{ $post->creador }}]]></author>
                 <guid>{{ $post->id }}</guid>
                 <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+                <description><![CDATA[<?php echo env('APP_ADMIN') . "/archivos/imagenes/" . $post['imgdestacado']; ?>]]></description>
             </item>
         @endforeach
     </channel>
